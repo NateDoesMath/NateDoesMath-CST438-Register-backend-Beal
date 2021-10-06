a02945e16cbd9b6d1eed6e78d64e704b4f829fb4
NateDoesMath <natebeal18@gmail.com>
2021-10-05 18:45:29 -0700

    change config

--- src/test/java/com/cst438/JunitTestSchedule.java
+++ src/test/java/com/cst438/JunitTestSchedule.java
@@ -2,11 +2,9 @@ package com.cst438;
 
 import static org.mockito.ArgumentMatchers.any;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
-
-import java.util.Calendar;
-import java.util.List;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +12,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import com.cst438.domain.*;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -22,14 +21,10 @@ import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import java.util.Calendar;
+import java.util.List;
+
 import com.cst438.controller.ScheduleController;
-import com.cst438.domain.Course;
-import com.cst438.domain.CourseRepository;
-import com.cst438.domain.Enrollment;
-import com.cst438.domain.EnrollmentRepository;
-import com.cst438.domain.ScheduleDTO;
-import com.cst438.domain.Student;
-import com.cst438.domain.StudentRepository;
 import com.cst438.service.GradebookService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
@@ -156,7 +151,7 @@ public class JunitTestSchedule {
 				found = true;
 			}
 		}
-		assertEquals(true, found);
+		assertTrue("Added course not in updated schedule.", found);
 		
 		// verify that repository find method was called.
 		verify(enrollmentRepository, times(1)).findStudentSchedule(TEST_STUDENT_EMAIL, TEST_YEAR, TEST_SEMESTER);
@@ -208,11 +203,6 @@ public class JunitTestSchedule {
 	
 		// verify that repository delete method was called.
 		verify(enrollmentRepository).delete(any(Enrollment.class));
-	}
-	
-	@Test
-	public void createStudent()  throws Exception {
-	
 	}
 		
 	private static String asJsonString(final Object obj) {
