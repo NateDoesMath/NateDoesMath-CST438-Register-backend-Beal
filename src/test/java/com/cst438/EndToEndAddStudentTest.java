6b851fdc188d872e78d6bd297c5c33f3692b9e98
NateDoesMath <natebeal18@gmail.com>
2021-10-05 21:46:01 -0700

    change config

---
+++ src/test/java/com/cst438/EndToEndAddStudentTest.java
@@ -0,0 +1,118 @@
+package com.cst438;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.cst438.domain.Course;
+import com.cst438.domain.CourseRepository;
+import com.cst438.domain.Enrollment;
+import com.cst438.domain.EnrollmentRepository;
+import com.cst438.domain.Student;
+import com.cst438.domain.StudentRepository;
+
+
+public class EndToEndAddStudentTest {
+
+	 public static final String CHROME_DRIVER_FILE_LOCATION = "/C:/Users/Ass/Desktop/chromedriver.exe";
+
+    public static final String URL = "https://cst438-register-fe.herokuapp.com/";
+    
+    public static final String TEST_USER_EMAIL = "test@csumb.edu";
+    
+    public static final String TEST_USERNAME = "test";
+    
+    public static final int TEST_ID = 2;
+    
+    public static final String TEST_STATUS = "0";
+    
+    public static final int TEST_COURSE_ID = 40443;
+    
+    public static final int SLEEP_DURATION = 1000; // 1 second.
+    
+    @Autowired
+    EnrollmentRepository enrollmentRepository;
+
+    @Autowired
+    CourseRepository courseRepository;
+    
+    @Autowired
+    StudentRepository studentRepository;
+
+    @Test
+    public void createAssignmentTest() throws Exception {
+        
+//        Course c = new Course();
+//        c.setCourse_id(40443);
+//        c.setInstructor(TEST_INSTRUCTOR_EMAIL);
+//        c.setSemester("Fall");
+//        c.setYear(2021);
+//        c.setTitle("Test Course");
+//
+//        courseRepository.save(c);
+
+
+        // set the driver location and start driver
+        //@formatter:off
+        // browser    property name                 Java Driver Class
+        // edge     webdriver.edge.driver         EdgeDriver
+        // FireFox     webdriver.firefox.driver     FirefoxDriver
+        // IE         webdriver.ie.driver         InternetExplorerDriver
+        //@formatter:on
+
+        System.setProperty("webdriver.chrome.driver", CHROME_DRIVER_FILE_LOCATION);
+        WebDriver driver = new ChromeDriver();
+        // Puts an Implicit wait for 10 seconds before throwing exception
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        driver.get(URL);
+        Thread.sleep(SLEEP_DURATION);
+
+        try {
+            
+            // Locate and click New Assignment button
+            driver.findElement(By.xpath("//a[last()]")).click();
+            Thread.sleep(SLEEP_DURATION);
+            
+            // Locate text fields and enter the data
+            WebElement we = driver.findElement(By.xpath("//input[@name='studentName']"));
+            we.sendKeys("test");
+            we = driver.findElement(By.xpath("//input[@name='studentEmail']"));
+            we.sendKeys(TEST_USER_EMAIL);
+            we = driver.findElement(By.xpath("//input[@name='statusCode']"));
+            we.sendKeys(TEST_STATUS);
+            
+            
+            driver.findElement(By.xpath("//input[@name='submitButton']")).click();
+            Thread.sleep(SLEEP_DURATION);
+
+            //Verify to see if student is in database
+            Student s = studentRepository.findByEmail(TEST_USER_EMAIL);
+            assertNotNull(s.getEmail(), "email not found in database");
+
+        } catch (Exception ex) {
+            throw ex;
+        } finally {
+
+            //Delete 
+//            Assignment a = assignmentRepository.findByName(TEST_ASSIGNMENT_NAME).get(0);
+//            if (a!=null) assignmentRepository.delete(a);
+//            courseRepository.delete(c);
+
+            driver.quit();
+        }
+
+    }
+	
+}
--- src/test/java/com/cst438/EndToEndScheduleTest.java
+++ src/test/java/com/cst438/EndToEndScheduleTest.java
@@ -33,13 +33,13 @@ import com.cst438.domain.EnrollmentRepository;
 @SpringBootTest
 public class EndToEndScheduleTest {
 
-	public static final String CHROME_DRIVER_FILE_LOCATION = "C:/chromedriver_win32/chromedriver.exe";
+	public static final String CHROME_DRIVER_FILE_LOCATION = "C:/Users/Ass/Desktop/chromedriver.exe";
 
-	public static final String URL = "http://localhost:3000";
+	public static final String URL = "https://cst438-register-fe.herokuapp.com/";
 
 	public static final String TEST_USER_EMAIL = "test@csumb.edu";
 
-	public static final int TEST_COURSE_ID = 40442;
+	public static final int TEST_COURSE_ID = 40443;
 
 	public static final String TEST_SEMESTER = "2021 Fall";
 
