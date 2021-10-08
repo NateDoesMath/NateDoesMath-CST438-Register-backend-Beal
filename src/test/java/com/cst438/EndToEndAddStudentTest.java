6e6a403ba9e079c34be3496795b1fbe3ea6d5d5d
NateDoesMath <natebeal18@gmail.com>
2021-10-07 18:17:37 -0700

    added functionality

--- src/test/java/com/cst438/EndToEndAddStudentTest.java
+++ src/test/java/com/cst438/EndToEndAddStudentTest.java
@@ -101,7 +101,7 @@ public class EndToEndAddStudentTest {
             //Verify to see if student is in database
             Student s = studentRepository.findByEmail(TEST_USER_EMAIL);
             we = driver.findElement(By.xpath("//div[@data-field='name' and @data-value='" + s.getName() + "']"));
-            assertNotNull(s.getEmail(), "email not found in database");
+            assertNotNull(we, "email not found in database");
 
         } catch (Exception ex) {
             throw ex;
